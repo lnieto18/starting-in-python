076f0ae0d9fead990fec418293641b02d3aa1df4
Crespa <linamanigo@gmail.com>
2019-10-12 22:15:25 -0500

    This is my first commit, I'm creating Enteros.py, cul.

---
+++ Enteros.py
@@ -0,0 +1,20 @@
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#Suma
+print a + b
+#Resta
+print c - a
+#Multiplicacion
+print d * c
+#Exponente
+print c ** 2
+#Division
+print float(c) / a 
+#Division entera
+print c / a
+#Modulo 
+print 7%3
+
