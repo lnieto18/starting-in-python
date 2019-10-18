1ac573f208ab55374baab9eba857de06c33be1d6
Crespa <linamanigo@gmail.com>
2019-10-18 00:11:43 -0500

    Added tryal code with lists

---
+++ Listas.py
@@ -0,0 +1,17 @@
+l = [2, "tres", True,["uno", 10]]
+l2 = l[0]
+l3 = l[3][0]
+l[1] = 4
+l4 = l[0:3:2]
+l5 = l[1::2]
+l[0:2] = [4]
+l6 = l[-1]
+
+print l
+print l2
+print l3
+print l[1]
+print l4
+print l5
+print l[0:2]
+print l6
