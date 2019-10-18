27591d2c09506a1be61f8fc9612de63a1c2a91f0
Crespa <linamanigo@gmail.com>
2019-10-17 22:11:55 -0500

    Prueba cadenas

---
+++ Cadenas.py
@@ -0,0 +1,18 @@
+#Comillas Simples
+cads = 'Texto entre\ncomillas simples.\nTexto entre\tcomillas simples.'
+#Comillas Dobles
+cadd = r"Texto entre\ncomillas dobles."
+#Comillas Triples
+cadc = """ Texto linea 1
+linea 2
+linea 3
+linea 4
+.
+.
+.
+linea N
+"""
+
+print cads
+print cadd
+print cadc
