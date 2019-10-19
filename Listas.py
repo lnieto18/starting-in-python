3d39d6fb4fd43bc66e147fcc9a4a240e77292182
juanmsl <juanmsl_pk@hotmail.com>
2019-10-19 00:45:43 -0500

    Modified Listas.py

--- Listas.py
+++ Listas.py
@@ -15,3 +15,23 @@ print l4
 print l5
 print l[0:2]
 print l6
+
+tryal = [1,2,3,4,5,6,7,8,9,0]
+
+print tryal
+print len(tryal)
+
+tryal.append(11)
+
+print tryal
+print len(tryal)
+
+tryal.remove(5)
+indexof3 = tryal.index(3)
+
+print "index of 3 is " + str(indexof3)
+
+tryal.pop(indexof3)
+
+print tryal
+print len(tryal)
